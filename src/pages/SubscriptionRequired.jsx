d2e4e3bbd88e87c88696ec50d2ce1145e9b01206
kofi-lartey <kofilartey12@gmail.com>
2026-07-16 16:39:23 +0000

    sub fixed

--- src/pages/SubscriptionRequired.jsx
+++ src/pages/SubscriptionRequired.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import BrandLogo from '../components/common/BrandLogo';
 import { 
   FiCheck, FiCreditCard, FiStar, 
   FiZap, FiArrowRight, FiInfo,
